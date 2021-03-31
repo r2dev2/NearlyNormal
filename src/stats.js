b887cab6fc2b469a47e7d7b8477ea5a1a80b70c5
r2dev2 <ronak.badhe@gmail.com>
2021-03-30 18:09:08 -0700

    monte carlo simulation to get accurate p and ci

---
+++ src/stats.js
@@ -0,0 +1,38 @@
+const MC_SIMS = 1e4;
+
+function monteCarloSample(seq) {
+  const sample = [];
+  seq.forEach(sample.push(rngChoice(seq)));
+  return sample;
+}
+
+function mean(seq) {
+  return seq.reduce((sum, num) => sum + num) / seq.length;
+}
+
+function monteCarloMeans(seq) {
+  const means = [];
+  for (let i = 0; i < MC_SIMS; ++i) {
+    means.push(mean(monteCarloSample(seq)));
+  }
+  return means;
+}
+
+export function monteCarlo(seq) {
+  const means = monteCarloMeans().sort();
+  const { length: l } = means;
+  return {
+    // Use binary search later if this is too slow
+    p(condition) {
+      return means.reduce((sum, num) => sum + condition(num), 0) / l;
+    },
+    ci(level) {
+      const lr = (1 - level) / 2;
+      return { left: means[lr], right: means[l - lr] };
+    }
+  };
+}
+
+function rngChoice(seq) {
+  return seq[Math.floor(Math.random() * seq.length)];
+}
